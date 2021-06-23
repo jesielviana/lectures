d1f70be26f25a4e94c1a938897fe943365eca862
jesielviana <jesielviana@pm.me>
2021-06-23 17:59:49 -0300

    update class notes

--- Tópicos Especiais de Programação/3 - Paradigmas de programação/code/mutable.js
+++
@@ -1,8 +0,0 @@
-const list = [1, 2, 3]
-console.log(list) // [ 1, 2, 3 ]
-
-list.pop()
-console.log(list) // [ 1, 2 ]
-
-list.push(4)
-console.log(list) // [ 1, 2, 4 ]
--- Tópicos Especiais de Programação/3 - Paradigmas de programação/code/pure_functions.js
+++
@@ -1,5 +0,0 @@
-function getTotalPrice (value, fee) {
-  return value + value * fee
-}
-
-console.log(getTotalPrice(10, 0.1))
